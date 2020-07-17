dc5f8a57e0902b3b5f02800f3215fe5f7f2674bd
devCbssolutionsRo <dev@cbssolutions.ro>
2020-07-17 13:34:45 +0300

    now able to show also without prices

--- l10n_ro_stock_picking_comment_template/models/stock_picking.py
+++ l10n_ro_stock_picking_comment_template/models/stock_picking.py
@@ -6,6 +6,18 @@ class stock_picking(models.Model):
 
     delegate_id = fields.Many2one('res.partner', string='Delegate')
     mean_transp = fields.Char(string='Mean transport')
+# if the model stock_picking_repot_valued is is installed after this module will not work as intended
+# in this module we overwrite the field valued = fields.Boolean(related="partner_id.valued_picking", readonly=True) not to be partner related
+# we didn't put dependency on this module because that module is dependent on stock_account that is maybe not used
+    valued = fields.Boolean('Show stock values', default=True, readonly=False, help="show stock values only if is installed stock_picking_report_valued; and if is installed must be installed before this module")
+    installed_stock_picking_report_valued =fields.Boolean(compute="_compute_installed_stock_picking_report_valued", compute_sudo=True)
+    
+    def _compute_installed_stock_picking_report_valued(self):
+        stock_piging_report_valued = self.env['ir.module.module'].search([('name','=','stock_picking_report_valued')])
+        if stock_piging_report_valued.filtered(lambda r: r.state == 'installed'):
+            self.installed_stock_picking_report_valued = True
+        else:
+            self.installed_stock_picking_report_valued = False
 
     @api.onchange('delegate_id')
     def on_change_delegate_id(self):
