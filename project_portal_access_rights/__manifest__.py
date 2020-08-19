130b87beee4e8e8e3b90ef293feedae678b8d2bd
Mihai Fekete <feketemihai@odooerpromania.ro>
2020-08-19 11:32:38 +0300

    Add module for portal users access.

---
+++ project_portal_access_rights/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2020 NextERP Romania
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Projects Portal Access Rights",
+    "summary": "Add portal access rights to all project types",
+    "version": "13.0.1.0.0",
+    "development_status": "Mature",
+    "category": "Security",
+    "website": "https://nexterp.ro",
+    "author": "NextERP Romania SRL",
+    "maintainers": ["feketemihai"],
+    "license": "AGPL-3",
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+    "depends": [
+        "project",
+    ]
+}
---
+++ project_portal_access_rights/hooks.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2020 NextERP Romania
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import api, SUPERUSER_ID
+
+def post_init_hook(cr, registry):
+    """Loaded after installing the module."""
+
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        # Update portal ir.rules to allow access right to portal users
+        # on all project types.
+        # project.project_task_rule_portal
+        # project.project_project_rule_portal
+        new_task_domain = """[
+        '|',
+                ('project_id.message_partner_ids', 'child_of', [user.partner_id.commercial_partner_id.id]),
+                ('message_partner_ids', 'child_of', [user.partner_id.commercial_partner_id.id]),
+        ]"""
+        task_rule = env.ref("project.project_task_rule_portal")
+        task_rule.domain = new_task_domain
+
+        new_project_domain = """[
+                ('message_partner_ids', 'child_of', [user.partner_id.commercial_partner_id.id]),
+        ]"""
+        project_rule = env.ref("project.project_project_rule_portal")
+        project_rule.domain = new_project_domain
+
+
+def uninstall_hook(cr, registry):
+    """Loaded before uninstalling the module."""
+    # Update portal ir.rules to initial domain
+    # project.project_task_rule_portal
+    # project.project_project_rule_portal
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        old_task_domain = """[
+            '|',
+                '&',
+                    ('project_id.privacy_visibility', '=', 'portal'),
+                    ('project_id.message_partner_ids', 'child_of', [user.partner_id.commercial_partner_id.id]),
+                '&',
+                    ('project_id.privacy_visibility', '=', 'portal'),
+                    ('message_partner_ids', 'child_of', [user.partner_id.commercial_partner_id.id]),
+            ]"""
+        task_rule = env.ref("project.project_task_rule_portal")
+        task_rule.domain = old_task_domain
+        old_project_domain = """[
+                '&',
+                    ('privacy_visibility', '=', 'portal'),
+                    ('message_partner_ids', 'child_of', [user.partner_id.commercial_partner_id.id]),
+            ]"""
+        project_rule = env.ref("project.project_project_rule_portal")
+        project_rule.domain = old_project_domain
