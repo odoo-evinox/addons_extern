9921c643acdc3e0fb9df381945dfe9566a7b2fd0
teodoralexandru@nexterp.ro <teodoralexandru@nexterp.ro>
2020-10-09 14:42:55 +0300

    ok float fields in xlsx excel export

---
+++ xlsx_export_fix_float_style/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2020 NextERP Romania SRL
+
+{
+    "name": "In XLSX export at float fields put excel float style",
+    "summary": "In XLSX export at float fields put excel float style. The decimal places will be min from (3 and decimal places of the field from database)",
+    "version": "1.0",
+    "license": "AGPL-3",
+    "author": "NextERP Romania",
+    "website": "",
+    "depends": ["web"],
+    "data": [],
+}
