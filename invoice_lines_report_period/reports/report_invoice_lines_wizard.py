b56037a793f35f9a38f977da671e6811142c5493
teodoralexandru@nexterp.ro <teodoralexandru@nexterp.ro>
2020-10-10 15:46:26 +0300

    ok report vanzari html

--- invoice_lines_report_period/reports/report_invoice_lines_wizard.py
+++ invoice_lines_report_period/reports/report_invoice_lines_wizard.py
@@ -7,10 +7,15 @@ class ReportInvoiceLinesWizardGiveMoveLines(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
+        out_invoices_in_period = self.env['account.move'].search([('state','=','posted'),
+                                                                   ('type','in',['out_invoice','out_receipt']),
+                                                                   #('move_id.reversal_move_id','=',False), # wasn't reversed 
+                                                                    ])
         account_move_line_ids = self.env['account.move.line'].search([('date','>=',data['date_from']), 
                                                                       ('date','<=',data['date_to']),
                                                                       ('display_type', '=', False),
                                                                       ('exclude_from_invoice_tab', '=', False),
+                                                                      ('move_id', 'in', out_invoices_in_period.ids)
                                                                       ],order="product_id, date asc")
         sorted_account_move_line_ids = account_move_line_ids.sorted(key=lambda x: str( x.product_id.name))
         docargs = {
