bae3adb540ea8d9f9b8ae274165c633eb092a73d
teodoralexandru@nexterp.ro <teodoralexandru@nexterp.ro>
2020-10-11 21:52:01 +0300

    also filter per products

--- invoice_lines_report_period/reports/report_invoice_lines_wizard.py
+++ invoice_lines_report_period/reports/report_invoice_lines_wizard.py
@@ -11,15 +11,22 @@ class ReportInvoiceLinesWizardGiveMoveLines(models.AbstractModel):
                                                                    ('type','in',['out_invoice','out_receipt']),
                                                                    #('move_id.reversal_move_id','=',False), # wasn't reversed 
                                                                     ])
-        account_move_line_ids = self.env['account.move.line'].search([('date','>=',data['date_from']), 
-                                                                      ('date','<=',data['date_to']),
-                                                                      ('display_type', '=', False),
-                                                                      ('exclude_from_invoice_tab', '=', False),
-                                                                      ('move_id', 'in', out_invoices_in_period.ids)
-                                                                      ],order="product_id, date asc")
+        search_domain = [('date','>=',data['date_from']), 
+                          ('date','<=',data['date_to']),
+                          ('display_type', '=', False),
+                          ('exclude_from_invoice_tab', '=', False),
+                          ('move_id', 'in', out_invoices_in_period.ids)
+                          ]
+        filters = ""
+        if data['product_ids'][0][2]:
+            search_domain += [('product_id', 'in', data['product_ids'][0][2] )]
+            products = self.env['product.product'].search([('id', 'in', data['product_ids'][0][2])])
+            filters += "Only products: "+", ".join([p.name for p in products])
+        account_move_line_ids = self.env['account.move.line'].search(search_domain, order="product_id, date asc")
         sorted_account_move_line_ids = account_move_line_ids.sorted(key=lambda x: str( x.product_id.name))
         docargs = {
             'docs': self,
+            'filters':filters,
             'lines':sorted_account_move_line_ids,
         }
         return docargs
