01698d21389f36390421b7ce85c5456fd35de2c0
teodoralexandru@nexterp.ro <teodoralexandru@nexterp.ro>
2021-02-11 13:23:25 +0200

    like in pr https://github.com/OCA/stock-logistics-reporting/pull/124   why we didn't put it?

--- stock_card_report/__manifest__.py
+++ stock_card_report/__manifest__.py
@@ -4,18 +4,18 @@
 {
     "name": "Stock Card Report",
     "summary": "Add stock card report on Inventory Reporting.",
-    "version": "1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-reporting",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["stock", "date_range", "report_xlsx_helper"],
     "data": [
+        "security/ir.model.access.csv",
         "data/paper_format.xml",
         "data/report_data.xml",
         "reports/stock_card_report.xml",
         "wizard/stock_card_report_wizard_view.xml",
-        "security/ir.model.access.csv",
     ],
     "installable": True,
 }
