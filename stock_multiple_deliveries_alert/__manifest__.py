409bfd155097f179ba9fb787bc573ba430d34a6e
Nexterp <dev@cbssolutions.ro>
2021-07-24 16:09:28 +0300

    stock_multiple_deliveries_alert also on sale show has unprocessed deliveries

--- stock_multiple_deliveries_alert/__manifest__.py
+++ stock_multiple_deliveries_alert/__manifest__.py
@@ -5,12 +5,20 @@
 {
     "name": "Stock multiple deliveries alert",
     "summary": "This module is telling in picking if are more deliveries to same customer",
-    "version": "14.0.0.1",
+    "version": "14.0.0.2",
     "license": "AGPL-3",
     "description":""" used if you have for example online shop and the customer is creating multiple orders. to know at delivery to make only one packet 
-    ( and with this info you can take whatever decision you want. without it you will make a lot of deliveries for same partner) """,
+    ( and with this info you can take whatever decision you want. without it you will make a lot of deliveries for same partner) 
+ 
+ v 0.2  also in sale you have a field called unprocess_delivery_ids meaning that exist at least one stock.picking that is not in ['draft', 'done', 'cancel']
+        you have some filed to tell you that exist other sales with unprocess_delivery_ids  or other that are processed and in less than 24h     
+    
+    
+    """,
     "author": "NextERP Romania",
     "website": "https://nexterp.ro",
-    "depends": ["stock"],
-    "data": ["views/stock_picking_views.xml"],
+    "depends": ["sale_stock"],
+    "data": ["views/stock_picking_views.xml",
+             "views/sale_order_views.xml",
+             ],
 }
