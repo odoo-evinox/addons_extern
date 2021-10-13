6703a823b69a87753fc65e6eaac9714c8c471d6c
Nexterp <dev@cbssolutions.ro>
2021-10-13 11:12:18 +0300

    better conditional delivery

--- conditional_delivery/models/sale_order.py
+++ conditional_delivery/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, registry, SUPERUSER_ID
 from odoo.exceptions import ValidationError
 import logging
 _logger = logging.getLogger(__name__)
@@ -8,19 +8,28 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     def _get_delivery_methods(self):
-        address = self.partner_shipping_id
         # searching on website_published will also search for available website (_search method on computed field)
+        address = self.partner_shipping_id
         original_carriers = self.env['delivery.carrier'].sudo().search([('website_published', '=', True)]).available_carriers(address)
         carriers_with_ok_domain = self.env['delivery.carrier']
-        for carrier in original_carriers:
-            if carrier.partner_domain:
-                try:
-                    ok_address = address.search([('id','=',address.id)]+safe_eval(carrier.partner_domain))
-                    if not ok_address:
-                        continue  # we are excluding this carrier address
-                except Exception as ex:
-                    _logger.error(f'at carrier ({carrier},carrier.name) partner_domain={carrier.partner_domain} gives error: {ex}')
-            carriers_with_ok_domain |= carrier
-            
+
+        # Use a new cursor to avoid rollback that could be caused by an upper method
+        db_name = self._cr.dbname
+        try:
+            db_registry = registry(db_name)
+            with db_registry.cursor() as cr:
+                env = api.Environment(cr, SUPERUSER_ID, {})
+
+                for carrier in original_carriers:
+                    if carrier.partner_domain:
+                        try:
+                            ok_address = address.with_env(env).search([('id','=',address.id)]+safe_eval(carrier.partner_domain))
+                            if not ok_address:
+                                continue  # we are excluding this carrier address
+                        except Exception as ex:
+                            _logger.error(f'at carrier ({carrier},carrier.name) partner_domain={carrier.partner_domain} gives error: {ex}')
+                    carriers_with_ok_domain |= carrier
+        except Exception as ex:
+            _logger.error(f'at in conditional_delivery gives error: {ex}')
         return carriers_with_ok_domain
 
