1599a2df25762a0dec9b56342f6daaf282e011f2
Cojocaru Marcel <marcel.cojocaru@gmail.com>
2022-12-05 13:20:48 +0200

    almas_14.0

--- nexterp_svl_recompute/wizard/stock_valuation_layer_recompute.py
+++ nexterp_svl_recompute/wizard/stock_valuation_layer_recompute.py
@@ -91,11 +91,6 @@ class StockValuationLayerRecompute(models.TransientModel):
             svl.new_remaining_value = svl.remaining_value        
             svl.new_remaining_qty = svl.remaining_qty        
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
 # self._cr.execute("""
 #     DELETE from stock_valuation_layer where valued_type is null
 # """)
@@ -222,10 +217,6 @@ class StockValuationLayerRecompute(models.TransientModel):
 # self._cr.commit()
 
 
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     def action_start_recompute(self):
         if self.product_id:
             products = self.product_id
